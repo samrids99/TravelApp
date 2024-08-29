1192b993757b682c5cd49dcba51143ede0ee5f57
samrids99 <samridsdale108@gmail.com>
2024-08-29 11:03:26 +0100

    added the DbContext class for entity framework

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,7 +2,7 @@ namespace API.Entities;
 
 public class AppUser
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // represents database columns and id automatically set to primary key
     public string Username { get; set; }
 
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ed60995ca75037fc1a7f49f7cb314d0845d44c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
