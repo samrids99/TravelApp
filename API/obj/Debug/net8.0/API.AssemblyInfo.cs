848763ff559b29ede9eda0d2de46fa783708b862
samrids99 <samridsdale108@gmail.com>
2024-08-29 12:39:11 +0100

    git ignore added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd1b899207b2894405df4d69fd632358243d93c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b04740bec189596e2bf9671f46c1b6547a1bde")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
