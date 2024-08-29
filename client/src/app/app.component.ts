67d045d10dadc8c70f2253e7a118562ed0918b67
samrids99 <samridsdale108@gmail.com>
2024-08-29 14:31:18 +0100

    how to make http requests with angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,11 +9,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  title = 'trips pending...';
+  users: any;
 
   constructor(private http: HttpClient) {
 
-   }
+  }
 
-  title = 'trips pending...';
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users')
+      .subscribe({
+        next: (users) => {
+          this.users = users;
+        },
+        complete: () => {}
+      })
+  }
+
 }
