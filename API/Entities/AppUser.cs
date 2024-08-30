d2beb2a3f46139fabe61d1f932059823309854b0
samrids99 <samridsdale108@gmail.com>
2024-08-30 13:48:23 +0100

    updated user entity to accommodate password hash & salt + migrated

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ public class AppUser
 {
     public int Id { get; set; } // represents database columns and id automatically set to primary key
     public string Username { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 
 }
