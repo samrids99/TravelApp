d3d96ee57eab8534010fb6fbeb60bbe9f84eeb3c
samrids99 <samridsdale108@gmail.com>
2024-08-30 16:19:21 +0100

    comment added on register dto

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs;
 
-public class RegisterDto
+public class RegisterDto // this dto is used to give an object to the Register method in AccountController.cs rather than it taking in a bunch of parameters
 {
     [Required]
     public string Username { get; set; }
