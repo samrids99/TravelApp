6fc89ba4f708bc009edd56742498ead488ec9962
samrids99 <samridsdale108@gmail.com>
2024-08-30 10:13:02 +0100

    displayed fetched users in client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e423e741152c3ca8ef2bf93d652dc2cb67e815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a412eeb0f2a5f9f196b802d41cc65cdd07516f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
